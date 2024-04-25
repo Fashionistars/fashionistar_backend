891ae983c8debcbd9d40e5bea136a3f255789640
zichdan <zichdan1999@gmail.com>
2024-04-25 07:43:48 +0100

    AWS S3 bucket configuration

--- backend/settings.py
+++ backend/settings.py
@@ -257,13 +257,13 @@ SIMPLE_JWT = {
 
 
 JAZZMIN_SETTINGS = {
-    "site_title": "Desphixs",
-    "site_header": "Desphixs",
+    "site_title": "Fashionistar",
+    "site_header": "Fashionistar",
     "site_brand": "Modern Marketplace ",
     "site_icon": "images/favicon.ico",
     "site_logo": "images/logos/logo.jpg",
-    "welcome_sign": "Welcome To Desphixs",
-    "copyright": "All right reserved to Desphixs",
+    "welcome_sign": "Welcome To Fashionistar",
+    "copyright": "All right reserved to Fashionistar",
     "user_avatar": "images/photos/logo.jpg",
     "topmenu_links": [
         {"name": "Dashboard", "url": "home",
