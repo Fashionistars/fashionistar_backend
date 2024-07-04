020040ee8177ca968989a0c37480cd6c930cdeed
Beloved-vince <vinceoludare@gmail.com>
2024-07-04 19:42:44 +0100

    transaction successful

--- transaction/views.py
+++ transaction/views.py
@@ -36,55 +36,6 @@ env.read_env()
 rave = Rave(publicKey="", usingEnv=False)
 
 
-# class InitiatePayment(APIView):
-
-#     permission_classes = [IsAuthenticated]
-
-    # def post(self, request):
-    #     total_amount = request.data.get('total_amount')  # Change to POST for form data(if you are using templates) or request.data.get for JSON
-    #     email = request.data.get('email')
-    #     reference = str(uuid.uuid4())
-
-    #     user = request.user
-
-    #     if user.is_anonymous:
-    #         return Response({'error': 'User is not authenticated'}, status=400)
-
-    #     # Replace these with your actual Flutterwave details
-    #     flutterwave_url = "https://api.flutterwave.com/v3/payments"
-    #     secret_key = env("FLUTTERWAVE_SECRET_KEY")  #"your_flutterwave_secret_key_here"
-
-    #     payload = {
-    #         "tx_ref": reference,
-    #         "amount": total_amount,
-    #         "currency": "NGN",
-    #         "redirect_url": "http://127.0.0.1:8000/payment/callback",#(Note this url must be hosted)  # Replace with your callback URL
-    #         "payment_type": "card",
-    #         "customer": {
-    #             "email": email
-    #             }
-    #         }
-
-    #     headers = {
-    #         "Authorization": f"Bearer {settings.SECRET_KEY}",
-    #         "Content-Type": "application/json"
-    #     }
-
-    #     try:
-    #         payment = Payment(user=user, amount=total_amount, reference=reference, status="pending")
-    #         payment.save()
-
-    #         response = requests.post(flutterwave_url, json=payload, headers=headers)
-    #         response_data = response.json()
-    #         return Response(response_data, status=status.HTTP_200_OK)
-
-    #     except requests.exceptions.RequestException as err:
-    #         # Handle request exceptions
-    #         return Response({'error': 'Payment initiation failed'}, status=500)
-    #     except ValueError as err:
-    #         # Handle JSON decoding error
-    #         return Response({'error': 'Payment initiation failed'}, status=500)
-
 class PaymentCallback(APIView):
     def get(self, request):
         status = request.GET.get('status')
@@ -191,21 +142,5 @@ class InitiateNewPayment(APIView):
         except Exception as e:
             logger.exception("An error occurred: %s", e)
             return Response({"error": f"An error occurred {e}"}, status=500)
-    # Install with: pip install rave_python
 
 
-
-
-
-details = {
-    "card_number": '4556052704172643',
-    "cvv": '899',
-    "expiry_month": '01',
-    "expiry_year": '23',
-    "currency": 'NGN',
-    "amount": '7500',
-    "email": 'user@example.com',
-    "fullname": 'Flutterwave Developers',
-    "tx_ref": 'YOUR_PAYMENT_REFERENCE',
-    "redirect_url": 'https://example_company.com/success',
-}
