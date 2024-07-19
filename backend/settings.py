3d848e2490dc6b004065c9df9665cd1f9b8b74bb
Beloved-vince <vinceoludare@gmail.com>
2024-07-19 01:02:27 +0100

    fixomg

--- backend/settings.py
+++ backend/settings.py
@@ -63,15 +63,12 @@ INSTALLED_APPS = [
     'addon',
     'api',
     'admin_backend',
-<<<<<<< HEAD
     'ShopCart',
     'checkout',
     'notification',
     'createOrder',
 
-=======
     'transaction',
->>>>>>> 44c6635 (update transanctions app)
 
     # Third Party Apps
     'rest_framework',
