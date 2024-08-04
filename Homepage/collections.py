0df8604e04a8dd25da43e3587cc25ff566c9c20a
zichdan <zichdan1999@gmail.com>
2024-08-04 05:51:19 -0700

    ARRANGED THE HOME PAGE ROUTES FOR CATEGORY, BRAND, AND COLLECTOINS

--- Homepage/collections.py
+++ Homepage/collections.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAdminUser, AllowAny
 from django.shortcuts import get_object_or_404
-from .models import Collections, Category, Brand
-from .serializers import CollectionsSerializer, CategorySerializer, BrandSerializer
+from Homepage.models import Collections
+from Homepage.serializers import CollectionsSerializer
 
 class CollectionsListView(APIView):
     """
