dc96784a102ce889a026d45c192f0eb8c930d56c
zichdan <zichdan1999@gmail.com>
2026-01-18 11:58:38 +0100

    refactor(auth): implement async services, api views, and global exception handling

--- backend/settings.py
+++ backend/settings.py
@@ -356,7 +356,9 @@ REST_FRAMEWORK = {
     # },
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10  # Number of items per page
+    'PAGE_SIZE': 10,  # Number of items per page
+
+    'EXCEPTION_HANDLER': 'apps.common.exceptions.custom_exception_handler',
 }
 
 
